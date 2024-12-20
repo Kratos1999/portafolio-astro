f875d403e8a892f6a5656086d1e332306765b831
Moises Maestre <mmaestre@cobiscorp.com>
2024-12-20 17:11:56 -0500

    Se añade primera version de portafolio

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,astro}', // Asegúrate de incluir Astro y cualquier archivo relevante
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
